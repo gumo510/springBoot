6f4a52c819b0e43c7fdd1955dbc17fbb14a02426
gumo <hu.yong@intellif.com>
2021-12-13 19:19:07 +0800

    fix:: 使用Swagger官方 starter

--- src/main/java/com/gumo/demo/config/SwaggerConfig.java
+++ src/main/java/com/gumo/demo/config/SwaggerConfig.java
@@ -1,27 +1,13 @@
 package com.gumo.demo.config;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.gumo.demo.entity.UnLoadFileZip;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -31,27 +17,26 @@ import java.util.List;
 /**
  * @author gumo
  * @version 2.0.0
- * @date 2019年04月18日
  * @description: 项目swagger文档
- * 默认地址 ：http://127.0.0.1:8080/swagger-ui.html#
+ * 默认地址 ：http://127.0.0.1:8080/swagger-ui/
  * @see
  * @since JDK1.8
  **/
 @Configuration
 @EnableSwagger2
+//@Profile(value = {"dev"})   //只在dev环境下启用Swagger文档，而在prod环境下不想启用
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 //需要排除一些不必要的接口
                 //.apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.gumo.demo.config"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .globalOperationParameters(setHeaderToken());
+                .build();
     }
 
     private ApiInfo apiInfo() {
@@ -65,12 +50,39 @@ public class SwaggerConfig {
                 .build();
     }
 
-    private List<Parameter> setHeaderToken() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        tokenPar.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
-        return pars;
+    /**
+     * 使用Swagger整合Spring Security，实现访问需要登录认证的接口。
+     * @param
+     * @return
+     */
+    private List<SecurityScheme> securitySchemes() {
+        //设置请求头信息
+        List<SecurityScheme> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        result.add(apiKey);
+        return result;
     }
 
+    private List<SecurityContext> securityContexts() {
+        //设置需要登录认证的路径
+        List<SecurityContext> result = new ArrayList<>();
+        result.add(getContextByPath("/auth/.*"));
+        return result;
+    }
+
+    private SecurityContext getContextByPath(String pathRegex) {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(pathRegex))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference("Authorization", authorizationScopes));
+        return result;
+    }
 }
