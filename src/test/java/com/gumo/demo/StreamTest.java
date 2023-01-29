211d6cd0608194b5474a661718a85c28227df2d3
gumo <hu.yong@intellif.com>
2023-01-29 14:15:06 +0800

    fix::Stream 递归树结构

--- src/test/java/com/gumo/demo/StreamTest.java
+++ src/test/java/com/gumo/demo/StreamTest.java
@@ -1,6 +1,8 @@
 package com.gumo.demo;
 
 import cn.hutool.core.collection.ListUtil;
+import com.alibaba.fastjson.JSON;
+import com.gumo.demo.entity.Menu;
 import com.gumo.demo.entity.User;
 import org.junit.jupiter.api.Test;
 
@@ -96,7 +98,7 @@ public class StreamTest {
 
         List<List<User>> partition = ListUtil.partition(list, 500);
         partition.parallelStream().forEach(data ->{
-            clearDate(data);
+//            clearDate(data);
         });
 
     }
@@ -130,6 +132,58 @@ public class StreamTest {
         }
         // Map value求和
         Integer totalCount = summaryMap.values().stream().mapToInt(BigDecimal::intValue).sum();
+    }
+
+    /**
+     * 可能平常会遇到一些需求，比如构建菜单，构建树形结构，数据库一般就使用父id来表示，为了降低数据库的查询压力，
+     * 我们可以使用Java8中的Stream流一次性把数据查出来，
+     * 然后通过流式处理，我们一起来看看，代码实现为了实现简单，就模拟查看数据库所有数据到List里面。
+     */
+    @Test
+    public void test5() {
+
+        List<Menu> menus = Arrays.asList(
+                new Menu(1,"根节点",0),
+                new Menu(2,"子节点1",1),
+                new Menu(3,"子节点1.1",2),
+                new Menu(4,"子节点1.2",2),
+                new Menu(5,"根节点1.3",2),
+                new Menu(6,"根节点2",1),
+                new Menu(7,"根节点2.1",6),
+                new Menu(8,"根节点2.2",6),
+                new Menu(9,"根节点2.2.1",7),
+                new Menu(10,"根节点2.2.2",7),
+                new Menu(11,"根节点3",1),
+                new Menu(12,"根节点3.1",11)
+        );
+
+        //获取父节点
+        List<Menu> collect = menus.stream().filter(m -> m.getParentId() == 0).map(
+                (m) -> {
+                    m.setChildList(getChildrens(m, menus));
+                    return m;
+                }
+        ).collect(Collectors.toList());
+        System.out.println("-------转json输出结果-------");
+        System.out.println(JSON.toJSON(collect));
+    }
+
+    /**
+     * 递归查询子节点
+     * @param root  根节点
+     * @param all   所有节点
+     * @return 根节点信息
+     */
+    private List<Menu> getChildrens(Menu root, List<Menu> all) {
+        List<Menu> children = all.stream().filter(m -> {
+            return Objects.equals(m.getParentId(), root.getId());
+        }).map(
+                (m) -> {
+                    m.setChildList(getChildrens(m, all));
+                    return m;
+                }
+        ).collect(Collectors.toList());
+        return children;
     }
 
     public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
